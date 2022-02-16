99a03f752e08418a5992595d9381607f8538d781
Shubham Seth <shubhamsth1234@gmail.com>
2022-02-16 14:58:17 +0530

    [add] Shubham added UC8 of EmployeePayrollApp program

---
+++ js/EmployeePayrollApp.js
@@ -0,0 +1,8 @@
+/* UC8:- Ability to Set Event Listener on Salary Range to display appropriate value */
+const salary = document.querySelector('#salary');
+const output = document.querySelector('.salary—output');
+output.textContent = salary.value;
+salary.addEventListener('input', function() 
+{
+    output.textContent = salary.value; 
+});
